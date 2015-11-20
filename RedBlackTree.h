b822683ec75a36ba4093f4dc5fa340bf22feedbb
vp4655 <tino.100@hotmail.com>
2015-11-20 17:12:04 +0100

    Initial red-black tree setup.

---
+++ RedBlackTree.h
@@ -0,0 +1,14 @@
+//
+// Created by Valentino on 20.11.2015..
+//
+
+#ifndef BIOINFORMATICS_REDBLACKTREE_H
+#define BIOINFORMATICS_REDBLACKTREE_H
+
+
+class RedBlackTree {
+
+};
+
+
+#endif //BIOINFORMATICS_REDBLACKTREE_H
