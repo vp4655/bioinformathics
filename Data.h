eeba3b7ae9766b5ef591bb861729cc3fa944634b
Scepo93 <stipe46@gmail.com>
2016-01-14 21:09:44 +0100

    commented data.h

--- Data.h
+++ Data.h
@@ -1,12 +1,17 @@
+/* Stjepan Livacic
+ *
+ * Class created to store data in red-black tree node.
+ *   id - id of Node
+ *   r - number of blocks in left subtree
+ *   p - number of simbols in left subtree
+ *   NoX - number of number of occurrences of symbol X in this subtree
+ */
+
 #ifndef BIOINFORMATICS_DATA_H
 #define BIOINFORMATICS_DATA_H
 #include <iostream>
 using namespace std;
 class Data{
-// id - id of Node
-//r - number of blocks in left subtree
-//p - number of simbols in left subtree
-//NoX - number of number of occurrences of symbol X in this subtree
 private:
     unsigned long id;
     unsigned long p;
@@ -15,12 +20,12 @@ private:
     unsigned long NoG;
     unsigned long NoT;
 
-//getters and setters for all variables
+//constructor
 public:
     Data(unsigned long id1, unsigned long p1, unsigned long NoA1, unsigned long NoC1, unsigned long NoG1, unsigned long NoT1) : id(id1), p(p1), NoA(NoA1), NoC(NoC1), NoG(NoG1),
                                                                     NoT(NoT1) {}
 
-
+//getters and setters for all variables
 public:
     unsigned long getId(){
         return this->id;
